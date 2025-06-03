2b8074d91f9825bd335911d44b19fa1674b43a55
Tri Yuni Ayuningtyas <yuni75561@gmail.com>
2025-06-03 21:12:49 +0700

    manambahkan alur desain level dan tombol pause

---
+++ Assets/PauseManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+
+    private bool isPaused = false;
+
+    public void TogglePause()
+    {
+        isPaused = !isPaused;
+
+        if (isPaused)
+        {
+            Time.timeScale = 0f;
+            pauseMenuUI.SetActive(true);
+        }
+        else
+        {
+            Time.timeScale = 1f;
+            pauseMenuUI.SetActive(false);
+        }
+    }
+}
